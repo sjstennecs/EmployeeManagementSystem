20b27b85eae5775d7ec62687a2c53d9beb3aa0be
sjstennecs <Stevenstennercs@gmail.com>
2023-07-31 08:36:04 -0400

    Creating attributes for the Address class

--- EmployeeManagementSystem/Address.cs
+++ EmployeeManagementSystem/Address.cs
@@ -2,5 +2,11 @@
 {
     internal class Address
     {
+        private string StreetAddress {get;set;}
+        private string City { get;set;}
+        private string State { get ; set;}
+        private string ZipCode { get;set;}
+        private string Country { get;set;}
+
     }
 }
