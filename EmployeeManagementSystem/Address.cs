8615ad7a0cea84f21512cb8e7a9bf2219d157590
sjstennecs <Stevenstennercs@gmail.com>
2023-07-31 08:56:41 -0400

    Adding class attributes and methods to Employee, and creating PrintAddress method to Address class.

--- EmployeeManagementSystem/Address.cs
+++ EmployeeManagementSystem/Address.cs
@@ -2,11 +2,17 @@
 {
     internal class Address
     {
+        // Attributes for Address class for better organization and abstraction.
         private string StreetAddress {get;set;}
         private string City { get;set;}
         private string State { get ; set;}
         private string ZipCode { get;set;}
         private string Country { get;set;}
+
+        public string PrintAddress()
+        {
+            return $"{StreetAddress} {City}, {State} {ZipCode}, {Country}";
+        }
 
     }
 }
--- EmployeeManagementSystem/Employee.cs
+++ EmployeeManagementSystem/Employee.cs
@@ -13,5 +13,24 @@ namespace EmployeeManagementSystem
         private string FirstName { get; set;}
         private string LastName { get; set;}
         private Address Address { get; set;}
+        private string Email { get; set;}
+        private string PhoneNumber { get; set;}
+        private string Department { get; set;}
+        private string Position { get; set;}
+        private int Salary { get; set;}
+
+        // Class Methods
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public string DisplayInfo()
+        {
+            string infoString = $"Employee: {EmployeeID}\nLast Name: {LastName}, First Name: {FirstName}\nDepartment: {Department}, Position: {.Position}, Salary: {Salary}" +
+                $"\nAddress: {Address.PrintAddress}, Email: {Email}";
+            return infoString;
+        }
     }
 }
